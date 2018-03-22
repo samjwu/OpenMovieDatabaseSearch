2e9ef41fccb43756cd59463aa83bc2d6c345fb9d
unknown <swm1000@hotmail.com>
2018-03-21 21:18:04 -0400

    search movie arrow fxn

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(() => {
-
+    $("#searchform").on("submit", (event) => {
+        let movietitle = $("movietitle").val();
+        searchMovies(movietitle);
+        event.preventDefault();
+    });
 });
+
+function searchMovies(movietitle) {
+    axios.get("http://www.omdbapi.com?s" + movietitle).then((response) => {
+
+    }).catch((error) => {
+        
+    });
+}
