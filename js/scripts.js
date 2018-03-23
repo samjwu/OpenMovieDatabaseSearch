8b1dc0d5fc0838b056c5e0102e2595d4845721b2
unknown <swm1000@hotmail.com>
2018-03-22 21:41:08 -0400

    omdb api key and search

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(document).ready(() => {
     $("#searchform").on("submit", (event) => {
-        let movietitle = $("movietitle").val();
+        let movietitle = $("#movietitle").val();
         searchMovies(movietitle);
         event.preventDefault();
     });
 });
 
 function searchMovies(movietitle) {
-    axios.get("http://www.omdbapi.com?s" + movietitle).then((response) => {
-
+    axios.get("http://www.omdbapi.com/apikey=b69472c7&?s=" + movietitle).then((response) => {
+        console.log(response)
     }).catch((error) => {
-        
+        console.log(response)
     });
 }
